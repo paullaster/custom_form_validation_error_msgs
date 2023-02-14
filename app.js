9bef8f6a82a0ffeacb0d196bfebcc459f618cc87
paullaster <paullasterokoth98@gmail.com>
2023-02-14 08:59:23 +0300

    custom validation msgs test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const email = document.getElementById('mail');
 
 email.addEventListener('input', (event) => {
-    if(email.validity.typeMissmatch){
+    if(email.validity.typeMismatch){
        return email.setCustomValidity("Expect an email!");
     }
     email.setCustomValidity("");

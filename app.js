c1a042ef15f77add179949f8bd408c48623694fc
paullaster <paullasterokoth98@gmail.com>
2023-02-14 10:53:00 +0300

    custom validation msgs test

--- app.js
+++ app.js
@@ -1,59 +1,53 @@
-const form = document.querySelector('form');
-const email = document.getElementById('mail');
-const emailError = document.querySelector('#mail + span.error');
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
 
-email.addEventListener('input', (event) => {
-    if(email.validity.valid){
-        console.log(email.validity)
-        emailError.textContent ='';
-        emailError.className='error';
-       //email.setCustomValidity("Expect an email!");
-       return;
-    }
-    console.log(email.validity)
-    showError();
-    //email.setCustomValidity("");
-
+email.addEventListener("input", event => {
+  if (email.validity.valid) {
+    console.log(email.validity);
+    emailError.textContent = "";
+    emailError.className = "error";
+    //email.setCustomValidity("Expect an email!");
+    return;
+  }
+  console.log(email.validity);
+  showError();
+  //email.setCustomValidity("");
 });
 
-const childrenArray = Array.from(form.childNodes)
-    childrenArray.forEach((child) => {
-        if(child.children){
-            const decendantsArray = Array.from(child.children)
-            
-            const decendantInput = decendantsArray.find(
-                (child) => {
-                    return child.nodeName === 'INPUT'
-                }
-            )
-            console.log(decendantInput)
-        }
-        return;
-    })
+const childrenArray = Array.from(form.childNodes);
+childrenArray.forEach(child => {
+  if (child.children) {
+    const decendantsArray = Array.from(child.children);
 
-form.addEventListener('submit', (event) => {
-    //console.log(childrenArray)
-    if(!email.validity.valid){
-        showError();
-        event.preventDefault();
-    }
-})
+    const decendantInput = decendantsArray.find(child => {
+      return child.nodeName === "INPUT";
+    });
+    console.log(decendantInput);
+  }
+  return;
+});
 
+form.addEventListener("submit", event => {
+  //console.log(childrenArray)
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
 
 const showError = () => {
-    if(email.validity.valueMissing){
-        emailError.innerHTML = "Email is missing!";
-
-    }else if (email.validity.typeMismatch){
-        emailError.innerHTML = "Please enter a valid email!";
-
-    }else if(email.validity.tooShort){
-        emailError.innerHTML = `
+  if (email.validity.valueMissing) {
+    emailError.innerHTML = "Email is missing!";
+  } else if (email.validity.typeMismatch) {
+    emailError.innerHTML = "Please enter a valid email!";
+  } else if (email.validity.tooShort) {
+    emailError.innerHTML = `
         Email too short!
         You've entered a ${email.value.length} 
-        ${email.value.length > 1 ? "characters": "character"} long email.
+        ${email.value.length > 1 ? "characters" : "character"} long email.
         Minimum length required: ${email.minLength} characters
         `;
-    }
-    emailError.className = "error active";
-}
+  }
+  emailError.className = "error active";
+};

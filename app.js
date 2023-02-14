c31827594a9005c05e4218bee78b02539dc21c98
paullaster <paullasterokoth98@gmail.com>
2023-02-14 09:58:08 +0300

    custom validation msgs test

--- app.js
+++ app.js
@@ -2,24 +2,6 @@ const form = document.querySelector('form');
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
 
-const showError = () => {
-    if(emailError.validity.valueMissing){
-        emailError.innerHTML = "Email is missing!";
-
-    }else if (email.validity.typeMismatch){
-        emailError.innerHTML = "Please enter a valid email!";
-
-    }else if(email.validity.tooShort){
-        emailError.innerHTML = `
-        Email too short!
-        You've entered a ${email.value.length} 
-        ${email.value.length > 1 ? "characters": "character"} long.
-        Minimum length required: ${email.minLength} characters
-        `;
-    }
-    emailError.className = "error active";
-}
-
 email.addEventListener('input', (event) => {
     if(email.validity.valid){
         console.log(email.validity)

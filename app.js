839236ac45f080948c523d7b40ed777ed2ec07f5
paullaster <paullasterokoth98@gmail.com>
2023-02-14 10:24:42 +0300

    custom validation msgs test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ email.addEventListener('input', (event) => {
 });
 
 form.addEventListener('submit', (event) => {
-    
+    const childrenArray = Array.from(form.children)
     if(!email.validity.valid){
         showError();
         event.preventDefault();
